f3a4ed45e72ef71d2f996df629c784b7c347b0eb
Ken 🏈 <me163@mac.com>
2024-08-08 14:25:11 -0700

    simple proof of concept

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,5 +1,5 @@
 use auth_service::{routes::SignupResponse, ErrorResponse};
-
+use rstest::*;
 use crate::helpers::{get_random_email, TestApp};
 
 #[tokio::test]
@@ -52,46 +52,35 @@ async fn should_return_201_if_valid_input() {
     app.clean_up().await;
 }
 
+#[rstest]
+#[case("woiefjwoeijf", "password123", true)]
+#[case("", "password123", true)]
 #[tokio::test]
-async fn should_return_400_if_invalid_input() {
-    // The signup route should return a 400 HTTP status code if an invalid input is sent.
-    // The input is considered invalid if:
-    // - The email is empty or does not contain '@'
-    // - The password is less than 8 characters
-
-    // Create an array of invalid inputs. Then, iterate through the array and
-    // make HTTP calls to the signup route. Assert a 400 HTTP status code is returned.
+async fn should_return_400_if_invalid_input(#[case] email: &'static str, #[case] password: &'static str, #[case] requires_2_fa: bool) {
     let mut app = TestApp::new().await;
-    let test_cases = [
-        serde_json::json!({
-            "password": "password123",
-            "requires2FA": true,
-            "email": "woiefjwoeijf",
-        }),
-        serde_json::json!({
-            "password": "password123",
-            "requires2FA": true,
-            "email": "",
-        }),
-    ];
-    for test_case in test_cases.iter() {
-        let response = app.post_signup(test_case).await; // call `post_signup`
-        assert_eq!(
-            response.status().as_u16(),
-            400,
-            "Failed for input: {:?}",
-            test_case
-        );
+    let test_data = serde_json::json!({
+        "email": email,
+        "password": password,
+        "requires2FA": requires_2_fa,
+    });
 
-        assert_eq!(
-            response
-                .json::<ErrorResponse>()
-                .await
-                .expect("Could not deserialize response body to ErrorResponse")
-                .error,
-            "Invalid credentials".to_owned()
-        );
-    }
+    let response = app.post_signup(&test_data).await; // call `post_signup`
+    assert_eq!(
+        response.status().as_u16(),
+        400,
+        "Failed for input: {:?}",
+        test_data
+    );
+
+    assert_eq!(
+        response
+            .json::<ErrorResponse>()
+            .await
+            .expect("Could not deserialize response body to ErrorResponse")
+            .error,
+        "Invalid credentials".to_owned()
+    );
+
     app.clean_up().await;
 }
 
