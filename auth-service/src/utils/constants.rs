36f6298f7d8c713ef26c7c774912ce25d3a211f9
Ken 🏈 <me163@mac.com>
2024-08-08 15:13:22 -0700

    First pass at LazyLock

--- auth-service/src/utils/constants.rs
+++ auth-service/src/utils/constants.rs
@@ -1,45 +1,37 @@
 use dotenvy::dotenv;
-use lazy_static::lazy_static;
 use secrecy::Secret;
-use std::env as std_env;
+use std::{env as std_env, sync::LazyLock};
 
 // Define a lazily evaluated static. lazy_static is needed because std_env::var is not a const function.
-lazy_static! {
-    pub static ref JWT_SECRET: Secret<String> = set_token();
-    pub static ref DATABASE_URL: Secret<String> = set_database_url();
-    pub static ref REDIS_HOST_NAME: String = set_redis_host();
-    pub static ref POSTMARK_AUTH_TOKEN: Secret<String> = set_postmark_auth_token();
-}
-
-fn set_token() -> Secret<String> {
+pub static JWT_SECRET: LazyLock<Secret<String>> = LazyLock::new(|| {
     dotenv().ok(); // Load environment variables
     let secret = std_env::var(env::JWT_SECRET_ENV_VAR).expect("JWT_SECRET must be set.");
     if secret.is_empty() {
         panic!("JWT_SECRET must not be empty.");
     }
     Secret::new(secret)
-}
+});
 
-fn set_database_url() -> Secret<String> {
+pub static DATABASE_URL: LazyLock<Secret<String>> = LazyLock::new(|| {
     dotenv().ok(); // Load environment variables
     let secret = std_env::var(env::DATABASE_URL_ENV_VAR).expect("DATABASE_URL must be set.");
     if secret.is_empty() {
         panic!("DATABASE_URL must not be empty.");
     }
     Secret::new(secret)
-}
+});
 
-fn set_postmark_auth_token() -> Secret<String> {
+pub static REDIS_HOST_NAME: LazyLock<String> = LazyLock::new(|| {
+    dotenv().ok();
+    std_env::var(env::REDIS_HOST_NAME_ENV_VAR).unwrap_or(DEFAULT_REDIS_HOSTNAME.to_owned())
+});
+
+pub static POSTMARK_AUTH_TOKEN: LazyLock<Secret<String>> = LazyLock::new(|| {
     dotenv().ok();
     Secret::new(
         std_env::var(env::POSTMARK_AUTH_TOKEN_ENV_VAR).expect("POSTMARK_AUTH_TOKEN must be set."),
     )
-}
-
-fn set_redis_host() -> String {
-    dotenv().ok();
-    std_env::var(env::REDIS_HOST_NAME_ENV_VAR).unwrap_or(DEFAULT_REDIS_HOSTNAME.to_owned())
-}
+});
 
 pub mod env {
     pub const JWT_SECRET_ENV_VAR: &str = "JWT_SECRET";
